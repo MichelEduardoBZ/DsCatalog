4c1381224e4b1deb164b00fb0e50db45a532cf9e
Michel <90931413+MichelEduardoBZ@users.noreply.github.com>
2024-04-11 20:45:09 -0300

    ORM Mapping, database seeding

--- backend/dscatalog/src/main/java/com/michel/dscatalog/entities/Category.java
+++ backend/dscatalog/src/main/java/com/michel/dscatalog/entities/Category.java
@@ -2,13 +2,18 @@ package com.michel.dscatalog.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
+
+import org.hibernate.annotations.ManyToAny;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
@@ -29,6 +34,9 @@ public class Category implements Serializable {
 	
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant updatedAt;
+	
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>();
 	
 	public Category(){}
 		
@@ -78,6 +86,10 @@ public class Category implements Serializable {
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
+	}
+	
+	public Set<Product> getProducts() {
+		return products;
 	}
 
 	@Override
